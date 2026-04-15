c47439ed0cd67aa4b8461e816abe304c7c9afc6f
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-04-15 16:31:32 +0100

    Added firebase environment.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBARaR4TndmDCItZoQW2UZw9lvI4Vh2kDg',
+    authDomain: 'austinatts-site.firebaseapp.com',
+    projectId: 'austinatts-site',
+    storageBucket: 'austinatts-site.firebasestorage.app',
+    messagingSenderId: '646306789902',
+    appId: '1:646306789902:web:14b633821215efcb3ada5f',
+    measurementId: 'G-RN4R13LVXZ',
+  },
+};
